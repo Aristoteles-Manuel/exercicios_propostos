5b8028ec8c337a68d0e548a1f34e78227538c22e
Aristóteles Manuel <186409324+Aristoteles-Manuel@users.noreply.github.com>
2026-08-22 19:25:16 +0100

    ATUALIZAÇÃO  APRENDENDO ARROW FUNCTION

--- exArrowF.js
+++ exArrowF.js
@@ -19,9 +19,21 @@ console.log(maior(26,7))
 const maior = (a,b) =>{
     return a>b ?a :b
 }
-*/
+
 let lista = [2,5,8,10,13]
  let pares = lista.filter(numero=>{
     return numero % 2 === 0
  })
  console.log(pares)
+ let lista =[2,5,8,10,13]
+ let dobro = lista.map(numero =>{
+    return numero*2
+ })
+ console.log(dobro
+ )
+ */
+let lista = [2,5,8,10,13]
+let somar = lista.reduce((numero,acumular)=>{
+    return numero+acumular
+},0)
+console.log(somar)
